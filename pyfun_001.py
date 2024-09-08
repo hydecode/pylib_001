3f6b5262802fc3870ff56800f0a82ddb00739ef6
Hyde Miller <millerhyde@gmail.com>
2024-09-08 10:12:38 -0400

    Add files via upload
    Added Python function library.

---
+++ pyfun_001.py
@@ -0,0 +1,17 @@
+# This is an example Python function library.
+
+# creating a simple add function
+def add(a, b):
+    return a+b
+
+# creating a simple division function
+def divide(a, b):
+    return a/b
+
+# creating a simple odd_even function
+# to check if the number is odd or even
+def odd_even(n):
+    if n % 2 == 0:
+        return("Even")
+    else:
+        return("Odd")
